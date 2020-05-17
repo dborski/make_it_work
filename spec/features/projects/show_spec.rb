d839a57b5e27a28dff833c32132d331af0f5bf08
Derek Borski <derek.borski@gmail.com>
2020-05-17 10:13:06 -0600

    Create spec for adding existing contestant to project, skip spec

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -51,5 +51,25 @@ RSpec.describe "Project show page" do
 
       expect(page).to have_content(boardfit.contestants.average_years_of_experience.round(2))
     end 
+
+    xit "Has a form to add existing contesant to a project" do
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      new_contestant = Contestant.create(name: "New Contestant", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: boardfit.id)
+
+      visit "/projects/#{boardfit.id}"
+
+      fill_in :id, with: "#{new_contestant.id}"
+      click_button "Add Contestant to Project"
+
+      expect(boardfit.contestants.count_of_contestants).to eq(3)
+    end 
   end
 end
