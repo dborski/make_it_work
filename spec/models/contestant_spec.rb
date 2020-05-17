aee5a19791878fb9f954b939f2f44a77dd3bf1a9
Derek Borski <derek.borski@gmail.com>
2020-05-17 09:23:57 -0600

    Create count_of_contestants model method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it "count_of_contestants" do
+
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      contestants = Contestant.all
+
+      expect(contestants.count_of_contestants).to eq(4)
+    end 
+  end 
 end
